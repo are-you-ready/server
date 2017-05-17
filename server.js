20ff8773e1998869c85f1591cc68bab4b2153448
Markus Tran <thebluepillow@gmail.com>
2017-05-16 18:29:09 -0700

    add morgan logging

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const AYR_PORT = process.argv[2] || process.env.AYR_PORT || 3000;
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
